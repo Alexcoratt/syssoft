483219378fd43851061ff5e9176572fedd059439
Alexcoratt <alexsm.w@yandex.ru>
2024-12-17 23:34:01 +0300

    Add simple AST visualization

--- src/main.c
+++ src/main.c
@@ -1,19 +1,78 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "ast.h"
 #include "prs.h"
 
 
 // TODO: ask about memory deallocating with yyfree
 
+// auxillary method declarations
+
+void fprintAst(FILE *ofptr, const struct Ast *, size_t indentSize);
+void fprintAstToken(FILE *ofptr, const char *token, size_t indentSize);
+void fprintAstNode(FILE *ofptr, const struct AstNode *, size_t indentSize);
+
+char *createIndentLine(size_t indentSize);
+
+
+// definitions
 
 AstStack sourceItems = EMPTY_AST_STACK;
 
 
 int main(int argc, char **argv) {
     yyparse();
-    printf("source item count: %lu\nsource items: %p\n", sizeOfAstStack(sourceItems), sourceItems);
+
+    // TODO: replace AstStack of sourceItems with struct Ast
+    size_t srcItemCount = sizeOfAstStack(sourceItems);
+    printf("source item count: %lu\n", srcItemCount);
+
+    fprintAst(stdout, &sourceItems->ast, 0);
 
     termAstStack(&sourceItems);
 
     return 0;
+}
+
+void fprintAst(FILE *ofptr, const struct Ast *astPtr, size_t indentSize) {
+	if (astPtr->isToken) {
+		const char *token = astPtr->option.token;
+		fprintAstToken(ofptr, token, indentSize);
+	} else {
+		const struct AstNode *astNodePtr = &astPtr->option.node;
+		fprintAstNode(ofptr, astNodePtr, indentSize);
+	}
+}
+
+char *createIndentLine(size_t indentSize) {
+	char *indentLine = malloc((indentSize + 3) * sizeof(char));
+	memset(indentLine, '|', indentSize);
+	strcpy(indentLine + indentSize, "--");
+	return indentLine;
+}
+
+void fprintAstToken(FILE *ofptr, const char *token, size_t indentSize) {
+	char *indentLine = createIndentLine(indentSize);
+	fprintf(ofptr, "%s \'%s\'\n", indentLine, token);
+	free(indentLine);
+}
+
+void fprintAstNode(FILE *ofptr, const struct AstNode *astNodePtr, size_t indentSize) {
+	char *indentLine = createIndentLine(indentSize);
+
+	// TODO: make the function output node type as string
+	fprintf(ofptr, "%s %d\n", indentLine, astNodePtr->nodeType);
+
+	size_t newIndentSize = indentSize + 1;
+
+	for (
+		AstStack child = astNodePtr->children;
+		child != EMPTY_AST_STACK;
+		child = child->next
+	)
+		fprintAst(ofptr, &child->ast, newIndentSize);
+
+	free(indentLine);
 }
